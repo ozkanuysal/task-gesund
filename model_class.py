c991fe741ab303b2a80e2b33cbf228d41436898e
Özkan <61411331+ozkanuysal@users.noreply.github.com>
2024-04-09 15:05:23 +0000

    add file

---
+++ model_class.py
@@ -0,0 +1,17 @@
+import torch
+from torch import nn
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv3d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+    def forward(self, x):
+        x = self.double_conv(x)
+        return x
