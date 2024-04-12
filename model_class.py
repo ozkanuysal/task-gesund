2afe7dd2ff2c99e520c675d222a47a06deb000fc
Özkan <61411331+ozkanuysal@users.noreply.github.com>
2024-04-12 07:46:45 +0000

    3dunet model arch. init

--- model_class.py
+++ model_class.py
@@ -3,6 +3,13 @@ from torch import nn
 
 class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
+        """
+        Initializes the double convolutional layers with batch normalization and ReLU activation.
+
+        Parameters:
+            in_channels (int): Number of input channels.
+            out_channels (int): Number of output channels.
+        """
         super().__init__()
         self.double_conv = nn.Sequential(
             nn.Conv3d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
@@ -15,3 +22,221 @@ class DoubleConv(nn.Module):
     def forward(self, x):
         x = self.double_conv(x)
         return x
+    
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        """
+        Initializes the class with the given input and output channels.
+
+        Parameters:
+            in_channels (int): The number of input channels.
+            out_channels (int): The number of output channels.
+        """
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.MaxPool3d(kernel_size=2, stride=2),
+            DoubleConv(in_channels, out_channels)
+        )
+    def forward(self, x):
+        """
+        This function takes in a parameter x, passes it through the encoder, and returns the result.
+        """
+        x = self.encoder(x)
+        return x
+    
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, trilinear=True):
+        """
+        Initializes the class with the given input and output channels and a flag for trilinear interpolation.
+
+        Parameters:
+            in_channels (int): Number of input channels.
+            out_channels (int): Number of output channels.
+            trilinear (bool): Flag for trilinear interpolation, default is set to True.
+        """
+        super().__init__()
+        self.up = nn.ConvTranspose3d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.conv = DoubleConv(in_channels, out_channels)
+    def forward(self, x1, x2):
+        """
+        Perform forward pass through the network with the given input tensors.
+
+        Parameters:
+            x1 (torch.Tensor): Input tensor 1.
+            x2 (torch.Tensor): Input tensor 2.
+
+        Returns:
+            torch.Tensor: Output tensor after passing through the network.
+        """
+        x1 = self.up(x1)
+        diffX = x2.size()[4] - x1.size()[4]
+        diffY = x2.size()[3] - x1.size()[3]
+        diffZ = x2.size()[2] - x1.size()[2]
+        
+        x1 = torch.nn.functional.pad(x1, (diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2, diffZ // 2, diffZ - diffZ // 2))
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+    
+class UpP(nn.Module):
+    def __init__(self, in_channels, out_channels, trilinear=True):
+        """
+        Initializes the neural network layers for upsampling and double convolution.
+
+        Args:
+            in_channels (int): Number of input channels.
+            out_channels (int): Number of output channels.
+            trilinear (bool, optional): Whether to use trilinear interpolation for upsampling. Defaults to True.
+        """
+        super().__init__()
+        self.up = nn.ConvTranspose3d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.conv = DoubleConv(out_channels*3, out_channels)
+    def forward(self, x1, x2, x3):
+        x1 = self.up(x1)
+        # Make sure size data when you combine are the same size
+        diffX1 = x3.size()[4] - x1.size()[4]
+        diffY1 = x3.size()[3] - x1.size()[3]
+        diffZ1 = x3.size()[2] - x1.size()[2]
+        
+        diffX2 = x3.size()[4] - x2.size()[4]
+        diffY2 = x3.size()[3] - x2.size()[3]
+        diffZ2 = x3.size()[2] - x2.size()[2]
+        
+        x2 = torch.nn.functional.pad(x2, (diffX2 // 2, diffX2 - diffX2 // 2, diffY2 // 2, diffY2 - diffY2 // 2, diffZ2 // 2, diffZ2 - diffZ2 // 2))
+        x1 = torch.nn.functional.pad(x1, (diffX1 // 2, diffX1 - diffX1 // 2, diffY1 // 2, diffY1 - diffY1 // 2, diffZ1 // 2, diffZ1 - diffZ1 // 2))
+        x = torch.cat([x3, x2, x1], dim=1)
+        return self.conv(x)
+
+class UpPP(nn.Module):
+    def __init__(self, in_channels, out_channels, trilinear=True):
+        """
+        Initializes the object with the specified input and output channels, and trilinear flag.
+        """
+        super().__init__()
+        self.up = nn.ConvTranspose3d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.conv = DoubleConv(out_channels*4, out_channels)
+    def forward(self, x1, x2, x3, x4):
+        x1 = self.up(x1)
+        # Make sure size data when you combine are the same size
+        diffX1 = x4.size()[4] - x1.size()[4]
+        diffY1 = x4.size()[3] - x1.size()[3]
+        diffZ1 = x4.size()[2] - x1.size()[2]
+        
+        diffX2 = x4.size()[4] - x2.size()[4]
+        diffY2 = x4.size()[3] - x2.size()[3]
+        diffZ2 = x4.size()[2] - x2.size()[2]
+        
+        diffX3 = x4.size()[4] - x3.size()[4]
+        diffY3 = x4.size()[3] - x3.size()[3]
+        diffZ3 = x4.size()[2] - x3.size()[2]
+        
+        x3 = torch.nn.functional.pad(x3, (diffX3 // 2, diffX3 - diffX3 // 2, diffY3 // 2, diffY3 - diffY3 // 2, diffZ3 // 2, diffZ3 - diffZ3 // 2))
+        x2 = torch.nn.functional.pad(x2, (diffX2 // 2, diffX2 - diffX2 // 2, diffY2 // 2, diffY2 - diffY2 // 2, diffZ2 // 2, diffZ2 - diffZ2 // 2))
+        x1 = torch.nn.functional.pad(x1, (diffX1 // 2, diffX1 - diffX1 // 2, diffY1 // 2, diffY1 - diffY1 // 2, diffZ1 // 2, diffZ1 - diffZ1 // 2))
+        x = torch.cat([x4, x3, x2, x1], dim=1)
+        return self.conv(x)
+    
+class UpPPP(nn.Module):
+    def __init__(self, in_channels, out_channels, trilinear=True):
+        """
+        A function to initialize the object with the provided input and output channels along with the option to use trilinear interpolation.
+        Parameters:
+            in_channels (int): The number of input channels.
+            out_channels (int): The number of output channels.
+            trilinear (bool): Whether to use trilinear interpolation or not (default is True).
+        """
+        super().__init__()
+        self.up = nn.ConvTranspose3d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.conv = DoubleConv(out_channels*5, out_channels)
+    def forward(self, x1, x2, x3, x4, x5):
+        x1 = self.up(x1)
+        # Make sure size data when you combine are the same size
+        diffX1 = x5.size()[4] - x1.size()[4]
+        diffY1 = x5.size()[3] - x1.size()[3]
+        diffZ1 = x5.size()[2] - x1.size()[2]
+        
+        diffX2 = x5.size()[4] - x2.size()[4]
+        diffY2 = x5.size()[3] - x2.size()[3]
+        diffZ2 = x5.size()[2] - x2.size()[2]
+        
+        diffX3 = x5.size()[4] - x3.size()[4]
+        diffY3 = x5.size()[3] - x3.size()[3]
+        diffZ3 = x5.size()[2] - x3.size()[2]
+        
+        diffX4 = x5.size()[4] - x4.size()[4]
+        diffY4 = x5.size()[3] - x4.size()[3]
+        diffZ4 = x5.size()[2] - x4.size()[2]
+        
+        x4 = torch.nn.functional.pad(x4, (diffX4 // 2, diffX4 - diffX4 // 2, diffY4 // 2, diffY4 - diffY4 // 2, diffZ4 // 2, diffZ4 - diffZ4 // 2))        
+        x3 = torch.nn.functional.pad(x3, (diffX3 // 2, diffX3 - diffX3 // 2, diffY3 // 2, diffY3 - diffY3 // 2, diffZ3 // 2, diffZ3 - diffZ3 // 2))
+        x2 = torch.nn.functional.pad(x2, (diffX2 // 2, diffX2 - diffX2 // 2, diffY2 // 2, diffY2 - diffY2 // 2, diffZ2 // 2, diffZ2 - diffZ2 // 2))
+        x1 = torch.nn.functional.pad(x1, (diffX1 // 2, diffX1 - diffX1 // 2, diffY1 // 2, diffY1 - diffY1 // 2, diffZ1 // 2, diffZ1 - diffZ1 // 2))
+        x = torch.cat([x5, x4, x3, x2, x1], dim=1)
+        return self.conv(x)
+    
+class Out(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        """
+        Initializes the object with the given input and output channels.
+
+        Parameters:
+            in_channels (int): The number of input channels.
+            out_channels (int): The number of output channels.
+        """
+        super().__init__()
+        self.out_conv = nn.Conv3d(in_channels, out_channels, kernel_size=1)
+    def forward(self, x):
+        return self.out_conv(x)
+    
+
+
+class UNET3D(nn.Module):
+    def __init__(self, in_channels, out_channels, n_classes):
+        """
+        Initializes the UNet model with the specified number of input channels, output channels, and classes.
+        
+        Parameters:
+            in_channels (int): Number of input channels.
+            out_channels (int): Number of output channels.
+            n_classes (int): Number of classes.
+        """
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.n_classes = n_classes
+        
+        self.double_conv = DoubleConv(self.in_channels, self.out_channels)
+        # encoder downsamplers
+        self.down_1 = Down(self.out_channels, self.out_channels*2)
+        self.down_2 = Down(self.out_channels*2, self.out_channels*4)
+        self.down_3 = Down(self.out_channels*4, self.out_channels*8)
+        self.down_4 = Down(self.out_channels*8, self.out_channels*16)
+
+        # decoder upsamplers
+        self.up_4 = Up(self.out_channels*16, self.out_channels*8)
+        self.up_3 = Up(self.out_channels*8, self.out_channels*4)
+        self.up_2 = Up(self.out_channels*4, self.out_channels*2)
+        self.up_1 = Up(self.out_channels*2, self.out_channels)
+
+        # output
+        self.out = Out(self.out_channels, self.n_classes)
+        
+        self.dropout = nn.Dropout3d(0.5)
+    def forward(self, x):
+        # Encoder
+        en_1 = self.double_conv(x)
+        en_2 = self.down_1(en_1)
+        en_2 = self.dropout(en_2)
+        en_3 = self.down_2(en_2)
+        en_4 = self.down_3(en_3)
+        en_4 = self.dropout(en_4)
+        en_5 = self.down_4(en_4)      
+        # Decoder
+        de_4 = self.up_4(en_5, en_4)
+        print(de_4.shape)
+        de_4 = self.dropout(de_4)
+        de_3 = self.up_3(de_4, en_3)
+        de_2 = self.up_2(de_3, en_2)
+        de_2 = self.dropout(de_2)
+        de_1 = self.up_1(de_2, en_1)        
+        out = self.out(de_1)
+        return out
